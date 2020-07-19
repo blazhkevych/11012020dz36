e11aa9e06176016cdd76f2f45725fe0017d1f192
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2020-07-19 18:47:35 +0300

    Добавьте файлы проекта.

---
+++ 11012020dz36.cpp
@@ -0,0 +1,15 @@
+﻿/*
+* Пользователь 	вводит объем в бушелях.Перевести его 	в кварты.
+*/
+#include <iostream>
+#include <Windows.h>;
+using namespace std;
+int main()
+{
+	SetConsoleOutputCP(1251);
+	cout << "Введите объем в бушелях : \n";
+	int a;
+	cin >> a;
+	cout << "кварт = " << a * 32 << endl;
+	return 0;
+}
